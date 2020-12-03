a51eacfc39ad8436cd1fea38b6917a643baf7413
Elizabeth Poggie <elizabeth.poggie@mail.mcgill.ca>
2020-12-03 14:56:47 -0500

    modularity is low in all cases, sad

--- make_network.py
+++ make_network.py
@@ -47,6 +47,26 @@ def user_network(biden_data, trump_data, title):
     return G
 
 
+def supporter_user_network(data, label, title):
+    G = nx.Graph()
+    # chop down the graph for it to make more sense
+    # data = {k: data[k] for k in list(data)[:10]}
+    # First add the nodes. Each node is the individual. also add their ground truth
+    for user in data.keys():
+        G.add_node(user, value="Biden")
+    # Next add edges. Each edge between individuals indicates common subreddits.
+    for start in G.nodes():
+        for end in G.nodes():
+            if start != end and not (G.has_edge(start, end) or G.has_edge(end, start)):
+                # make edge the common users between these nodes
+                common = get_common_subreddits(data, start, end)
+                if common > 0:
+                    G.add_edge(start, end, weight=common)
+    # save to view in gephi
+    nx.write_gexf(G, "./user_data/graphs/" + title +"_graph.gexf")
+    return G
+
+
 def get_common_users(data, start, end):
     total_users = 0
     for user, subreddits in data.items():
@@ -74,6 +94,44 @@ def subreddit_network(data, title):
     # save to view in gephi
     nx.write_gexf(G, "./user_data/graphs/" + title + "_subreddit_graph.gexf")
     return G
+
+
+def print_girvan_newman_graph(G):
+    k = 2
+    count = 0
+    inc = 0
+    node_communities = dict()
+    communities_generator = com.girvan_newman(G)
+
+    for communities in itertools.islice(communities_generator, k):
+        inc += 1
+        if inc == k:
+            for community in communities:
+                count += 1
+                for node in community:
+                    node_communities[node] = count
+
+    ## Calculate modularity
+    mod = nx_comm.modularity(G, next(communities_generator))
+    print("Modularity:", mod)
+
+    nx.draw(G, node_color=list(node_communities.values()))  # ,with_labels=True)
+    plt.show()
+
+    values = dict()
+    i = 0
+    for node, data in G.nodes(data=True):
+        i += 1
+        values[i] = data['value']
+
+    labels_true = list(values.values())
+    labels_pred = list(node_communities.values())  # i hope this is it
+
+    nmi = normalized_mutual_info_score(labels_true, labels_pred)
+    print('NMI: {:.4f}'.format(nmi))
+
+    ari = adjusted_rand_score(labels_true, labels_pred)
+    print('ARI: {:.4f}'.format(ari))
 
 
 def print_louvain_algo(g):
@@ -112,6 +170,10 @@ def print_louvain_algo(g):
 
 def make_and_analyze_user_network(biden_data, trump_data):
     g = user_network(biden_data, trump_data, "All_Users")
+    print_louvain_algo(g)
+    g = supporter_user_network(biden_data, "Biden", "Biden_User")
+    print_louvain_algo(g)
+    g = supporter_user_network(trump_data, "Trump", "Trump_User")
     print_louvain_algo(g)
 
 
